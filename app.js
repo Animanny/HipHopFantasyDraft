7be3fe7f68f9fcea344076557830bafe10fcc3e3
Ani Srikanth <ani@Anis-MacBook-Pro.local>
2019-03-05 23:58:21 -0500

    make it better

--- app.js
+++ app.js
@@ -6,19 +6,6 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 });
 
-io.on("connection",(socket)=>{
-    console.log("New Connection!");
-
-    socket.on("disconnect",()=>{
-        console.log("A user has disconnected");
-    });
-    socket.on('chat message', (message)=>{
-        console.log(message);
-        io.emit('chat message', message);
-    });
-});
-
-
-app.listen(process.env.PORT || 3000,()=>{
+http.listen(process.env.PORT || 3000,()=>{
     console.log('listening on *:'+process.env.PORT);
 });
