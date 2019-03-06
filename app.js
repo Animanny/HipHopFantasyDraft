86fa408a8d8e1e5615d67099ef170a18a228388b
Ani Srikanth <ani@Anis-MacBook-Pro.local>
2019-03-05 23:26:28 -0500

    Heroku was acting up, let's try again, should be working now

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 });
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/index.html");
+io.on("connection",(socket)=>{
+    console.log("New Connection!");
+
+    socket.on("disconnect",()=>{
+        console.log("A user has disconnected");
+    });
+    socket.on('chat message', (message)=>{
+        console.log(message);
+        io.emit('chat message', message);
+    });
 });
 
-
-
-http.listen(process.env.PORT, ()=>{
-    console.log("listening on *:"+process.env.PORT);
+http.listen(process.env.PORT || 3000,()=>{
+    console.log('listening on *:'+process.env.POR);
 });
